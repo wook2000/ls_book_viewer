060ab03981192bc873d17ef6b660105383c75e1d
Wook Kim <h.wook.kim@gmail.com>
2024-03-15 15:12:18 -0700

    rb175 lesson3 complete

--- book_viewer.rb
+++ book_viewer.rb
@@ -1,6 +1,71 @@
+require "tilt/erubis"
 require "sinatra"
 require "sinatra/reloader"
 
+before do
+  @contents = File.readlines "data/toc.txt"  
+end
+
+helpers do
+  def in_paragraphs(text)
+    text.split("\n\n").map.with_index do |paragraph, index|
+      "<p id=paragraph#{index}>#{paragraph}</p>"
+    end.join
+  end
+
+  def highlight(text,term)
+    text.gsub(term, %(<strong>#{term}</strong>))
+  end
+end
+
+def each_chapter
+  @contents.each_with_index do |name, index|
+    number = index + 1
+    contents = File.read("data/chp#{number}.txt")
+    yield number, name, contents
+  end
+end
+
+def chapters_matching(query)
+  results = []
+
+  return results unless query
+
+  each_chapter do |number, name, contents|
+    matches = {}
+    contents.split("\n\n").each_with_index do |paragraph, index|
+      matches[index] = paragraph if paragraph.include?(query)
+    end
+    results << {number: number, name: name, paragraphs: matches} if matches.any?
+  end
+
+  results
+end
+
 get "/" do
-  File.read "public/template.html"
+  @title = "The Adventures of Sherlock Holmes"
+
+  erb :home
+end
+
+get "/chapters/:number" do
+  number = params[:number].to_i
+  chapter_name = @contents[number - 1]
+
+  redirect "/" unless (1..@contents.size).cover? number
+
+  @title = "Chapter #{number}: #{chapter_name}"
+  @chapter = File.read "data/chp#{number}.txt"
+
+  erb :chapter
+end
+
+get "/search" do
+  @results = chapters_matching(params[:query])
+
+  erb :search
+end
+
+not_found do
+  redirect "/"
 end
